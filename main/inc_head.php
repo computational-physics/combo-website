f2ce0a4364c72d68511b0e36b796072cadf3b403
aaHung <Landxh@gmail.com>
2015-12-04 20:05:07 +0800

    Update to bootstrap 3.3.6

--- main/inc_head.php
+++ main/inc_head.php
@@ -6,7 +6,7 @@ define("ROOTPATH", "/main");
 <link rel="shortcut icon" href="//www.cityu.edu.hk/favicon.ico" />
 <!-- /Icon -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="<?php echo ROOTPATH ?>/css/bootstrap.css" rel="stylesheet" media="screen">
+<link href="<?php echo ROOTPATH ?>/css/bootstrap.min.css" rel="stylesheet" media="screen">
 <link href="<?php echo ROOTPATH ?>/css/bootstrap-theme.css" rel="stylesheet" media="screen">
 <script src="<?php echo ROOTPATH ?>/js/jquery-1.10.2.min.js"></script>
 <script src="<?php echo ROOTPATH ?>/js/detect_ie.js"></script>
