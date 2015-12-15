05800f76d1a642cd3856328482386a05bb13cda0
aaHung <Landxh@gmail.com>
2015-12-16 01:01:23 +0800

    Update sliders

--- main/index.php
+++ main/index.php
@@ -34,20 +34,52 @@
 			<ol class="carousel-indicators">
 				<li data-target="#carousel-slider" data-slide-to="0" class="active"></li>
 				<li data-target="#carousel-slider" data-slide-to="1"></li>
-				<li data-target="#carousel-slider" data-slide-to="2"></li>
+                <li data-target="#carousel-slider" data-slide-to="2"></li>
+                <li data-target="#carousel-slider" data-slide-to="3"></li>
+                <li data-target="#carousel-slider" data-slide-to="4"></li>
+                <li data-target="#carousel-slider" data-slide-to="5"></li>
+                <li data-target="#carousel-slider" data-slide-to="6"></li>
+                <li data-target="#carousel-slider" data-slide-to="7"></li>
+				<li data-target="#carousel-slider" data-slide-to="8"></li>
+                <li data-target="#carousel-slider" data-slide-to="9"></li>
+                <li data-target="#carousel-slider" data-slide-to="10"></li>
 			</ol>
 
 			<!-- Wrapper for slides -->
 			<div class="carousel-inner">
-				<div class="item active">
-					<img src="img/slider2.jpg">
-				</div>
-				<div class="item">
-					<img src="img/slider1.jpg">
-				</div>
-				<div class="item">
-					<img src="img/slider3.jpg">
-				</div>
+                <div class="item active">
+                    <img src="img/slides/slide-colleague-com-sci.jpg">
+                </div>
+                <div class="item">
+                    <img src="img/slides/slide-hpc-workshop-dec-2015.jpg">
+                </div>
+                <div class="item">
+                    <img src="img/slides/slide-michael-greenwood.jpg">
+                </div>
+                <div class="item">
+                    <img src="img/slides/slide-cityu-30.jpg">
+                </div>
+                <div class="item">
+                    <img src="img/slides/slide-barph.jpg">
+                </div>
+                <div class="item">
+                    <img src="img/slides/slide-trtituvb.jpg">
+                </div>
+                <div class="item">
+                    <img src="img/slides/slide-jinhui-xinhong-won-ee.jpg">
+                </div>
+                <div class="item">
+                    <img src="img/slides/slide-cityu-info-day.jpg">
+                </div>
+                <div class="item">
+                    <img src="img/slider2.jpg">
+                </div>
+                <div class="item">
+                    <img src="img/slider1.jpg">
+                </div>
+                <div class="item">
+                    <img src="img/slider3.jpg">
+                </div>
 			</div>
 
 			<!-- Controls -->
