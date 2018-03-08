27d79f75050ca25734c36df4637bfc2b18c55720
Zhen Li <33314620+truth-zhenli@users.noreply.github.com>
2018-03-08 11:37:01 +0800

    Publication Update

--- main/publications/index.php
+++ main/publications/index.php
@@ -31,77 +31,107 @@
                         <br>
                         <ol id="publication-list" reversed>
                             <li>Yonghui Zhang, Chunyi Zhi, and Jun Fan*, Lipid Adsorption Induced by Boron Nitride Nanosheet in mimic gram-negative bacterial and mammalian bilayer systems, submitted</li>
-
+							<br>
+							
                             <li>Huanbo Jiang, Zhiyao Xie, Zhen Li, V. K. Sharma, Jun Fan*, and Xiaolin Cheng, Microscopic Insights into Melittin Induced Changes in Molecular Dynamics of Lipid Bilayer Membranes: Role of Physical State and Composition of the Bilayers, in revision</li>
-
+							<br>
+							
 							<li>Yifan Ye, Yanhui Zhang, Q.F. He, Yu Zhuang, S. Wang, S.Q. Shi, J. Fan, A. Hu, Yong Yang, Atomic-Scale Distorted Lattice in Chemically Disordered Equimolar Complex Alloys, Acta Materialia, Accepted.</li>
+							<br>
 							
                             <li>Zhen Li, Yonghui Zhang, Chun Chan, Chunyi Zhi, Xiaolin Cheng, and Jun Fan*, Temperature-Dependent Lipid Extraction from Membranes by Boron Nitride Nanosheets, ACS Nano, Accepted.</li>
-
+							<br>
+							
                             <li>Jia Le Ma, Qiangqiang Meng, and Jun Fan*, Charge Driven Lateral Structural Evolution of Ions in Electric Double Layer Capacitor Strongly Correlates with Differential Capacitance, <a href="http://pubs.rsc.org/en/content/articlelanding/2018/cp/c7cp08075a#!divAbstract">Phys. Chem. Chem. Phys., 10.1039/C7CP08075A (2018) </a></li>
 								<img class="img-responsive center-block" src="../img/paperlist/2018PCCP2.jpg">
-
+							<br>
+							
                             <li>Qiangqiang Meng, Jiale Ma, Alice Hu, Chunyi Zhi, and Jun Fan*, The S-functionalized Ti3C2 Mxene as a High Capacity Electrode Material for Na-ion Batteries: A DFT Study, <a href="http://pubs.rsc.org/en/content/articlelanding/2018/nr/c7nr07649e">Nanoscale, 10: 3385-3392 (2018)</a></li>
 								<img class="img-responsive center-block" src="../img/paperlist/2018Nanoscale1.jpg">
+							<br>
 							
                             <li>Yonghui Zhang, Zhen Li, Chun Chan, Jiale Ma, Chunyi Zhi, Xiaolin Cheng, and Jun Fan*, Ordering of Lipid Membranes Altered by Boron Nitride Nanosheet, <a href="http://pubs.rsc.org/en/content/articlelanding/2017/cp/c7cp07136a#!divAbstract">Phys. Chem. Chem. Phys., 20: 3903-3910 (2018)</a>, inside cover</li>
 								<img class="img-responsive center-block" src="../img/paperlist/2018PCCP1.jpg">
-
+							<br>
+							
 							<li>Yanhua Zhao, Yang Wu, Liang Wang, Manman Zhang, Xuan Chen, Minjie Liu, Jun Fan, Junqiu Liu, Feng Zhou, Zuankai Wang. Bio-inspired reversible underwater adhesive. <a href="https://www.nature.com/articles/s41467-017-02387-2">Nat. Commun., 8: 2218 (2017)</a>. Selected by The Latest Science as the paper of the month (January 2018)</li>
 								<img class="img-responsive center-block" src="../img/paperlist/2017NC1.jpg">
+							<br>
 							
                             <li>Qiangqiang Meng, Alice Hu, Chunyi Zhi, and Jun Fan*, Theoretical prediction of MXene-like structure Ti3C4 as a high capacity electrode material for Na ion batteries, <a href="http://pubs.rsc.org/en/content/articlehtml/2017/CP/C7CP06171D">Phys. Chem. Chem. Phys., 19: 29106-29113 (2017)</a>, This article is part of the themed collection: <a href="http://pubs.rsc.org/en/journals/articlecollectionlanding?sercode=cp&themeid=6f8bc19b-215a-4535-b504-cc4450521837">2017 PCCP HOT Articles</a></li>
 								<img class="img-responsive center-block" src="../img/paperlist/2017PCCP1.jpg">
+							<br>
 							
                         	<li>Chun Chan, Lanyuan Lu, Fei Sun, Jun Fan*, Molecular Details of the PH Domain of ACAP1 BAR-PH Protein Binding to PIP-Containing Membrane, , <a href="http://pubs.acs.org/doi/abs/10.1021/acs.jpcb.6b09563">J. Phys. Chem. B, 121: 3586-3596 (2017)</a></li>
 								<img class="img-responsive center-block" src="../img/paperlist/2017JPCB1.jpg">
+							<br>
 							
                         	<li>Xuebo Quan, Chunwang Peng, Daohui Zhao, Libo Li, Jun Fan, and Jian Zhou, Molecular Understanding of the Penetration of Functionalized Gold Nanoparticles into Asymmetric Membranes,  <a href="http://pubs.acs.org/doi/abs/10.1021/acs.langmuir.6b02937">Langmuir, 33: 361 (2017)</a>, Cover Story</li> 
 								<img class="img-responsive center-block" src="../img/paperlist/2017Langmuir1.jpg">
+							<br>
 							
                         	<li>Y. F. Ye, Xiaodi Liu, Song Wang, Jun Fan, Chain Tsuan LIU, and Yong Yang, The Kinetic Origin of Delayed Yielding in Metallic Glasses, <a href="http://aip.scitation.org/doi/10.1063/1.4954376">Appl. Phys. Lett., 108: 251901 (2016)</a></li>
-
+							<br>
+							
                             <li>S. Lan, M. Blodgett, K. F. Kelton, J. L. Ma, J. Fan and X.-L. Wang, Structural crossover in a supercooled metallic liquid and the link to a liquid-to-liquid phase transition, <a href="http://dx.doi.org/10.1063/1.4952724">Appl. Phys. Lett., 108: 211907 (2016)</a></li>
-
+							<br>
+							
                         	<li>Chun Chan, Jun Fan, Andrew Messer, Steve Marston, Hiroyuki Iwamoto, Julien Ochala, Myopathy-inducing mutation H40Y in ACTA1 hampers actin filament structure and function, <a href="http://www.sciencedirect.com/science/article/pii/S0925443916300904">Biochim. Biophys. Acta, Mol. Basis Dis., 1862: 1453-8 (2016)</a></li>
-
+							<br>
+							
                             <li>Feng Wang, Jiale Ma, Fangyan Xie, Linkai Li, Jian Chen, Jun Fan and Ni Zhao, "Organic Cation-Dependent Degradation Mechanism of Organo Tin Halide Perovskites", <a href="http://dx.doi.org/10.1002/adfm.201505127">Adv. Funct. Mater., 26: 3417 (2016)</a></li>
-
+							<br>
+							
                             <li>Xian Chen, Limin Jin, Wei Kong, Tianying Sun, Wenfei Zhang, Xinhong Liu, Jun Fan, Siu Fung Yu, and Feng Wang, Confining energy migration in upconversion nanoparticles towards deep ultraviolet lasing, <a href="http://www.nature.com/ncomms/2016/160107/ncomms10304/full/ncomms10304.html">Nat. Commun., 7: 10304 (2016)</a></li> 
-
+							<br>
+							
                             <li>Chun Chan, Haohua Wen, Lanyuan Lu, and Jun Fan*, Multiscale molecular dynamics simulations membrane remodeling by BAR family proteins, <a href="http://cpb.iphy.ac.cn/EN/abstract/abstract66287.shtml">Chinese Physics B, 25: 018707 (2016)</a>, invited review</li>
+							<br>
 
                             <li>Yifan Ye, Song Wang, Jun Fan, Chain-Tsuan Liu, and Yong Yang, Atomistic mechanism of elastic softening in metallic glass under cyclic loading revealed by molecular dynamics simulations, <a href="http://www.sciencedirect.com/science/article/pii/S0966979515300649">Intermetallics 68: 5 (2016)</a></li>
-
+							<br>
+							
                             <li>Qi Wu, Bolong Huang, T. A. Niehaus, Xiaojing Yang, Jun Fan* and Rui-Qin Zhang: The role of tryptophans in the UV-B absorption of a UVR8 photoreceptor - a computational study. <a href="http://pubs.rsc.org/en/Content/ArticleLanding/2015/CP/C4CP06073C#!divAbstract">Phys. Chem. Chem. Phys., 17: 10786-10794 (2015)</a></li>
-
+							<br>
+							
                             <li>Xiaoli Zeng, Zhong Ren, Qi Wu, Jun Fan, Pan-Pan Peng, Kun Tang, Ruiqin Zhang, Kai-Hong Zhao, and Xiaojing Yang: Dynamic Crystallography Reveals Early Signaling Events in Ultraviolet Photoreceptor UVR8. <a href="http://www.nature.com/articles/nplants20146">Nature Plants, 1: 14006 (2015)</a></li>
-
-        					<li>
-                                Xiaoyun Pang, Jun Fan, Yan Zhang, Kai Zhang, Bingquan Gao, Jun Ma, Jian Li, Yuchen Deng, Qiangjun Zhou, Edward H. Egelman, Victor W. Hsu, Fei Sun: A PH Domain in ACAP1 Possesses Key Features of the BAR Domain in Promoting Membrane Curvature, <a href="http://www.sciencedirect.com/science/article/pii/S1534580714005504">Dev. Cell, 31: 73-86 (2014)</a>
+							<br>
+							
+        					<li>Xiaoyun Pang, Jun Fan, Yan Zhang, Kai Zhang, Bingquan Gao, Jun Ma, Jian Li, Yuchen Deng, Qiangjun Zhou, Edward H. Egelman, Victor W. Hsu, Fei Sun: A PH Domain in ACAP1 Possesses Key Features of the BAR Domain in Promoting Membrane Curvature, <a href="http://www.sciencedirect.com/science/article/pii/S1534580714005504">Dev. Cell, 31: 73-86 (2014)</a>
                                 <i>Highlighted in <a href="http://www.sciencedirect.com/science/article/pii/S1534580714006224">A Novel Twist in Membrane dePHormation</a></i>
                             </li>
-
+							<br>
+							
         					<li>Heqin Huang, Rui Chen, Jiale Ma, Li Yan, Yingqi Zhao, Yu Wang, Wenjun Zhang, Jun Fan*, and Xianfeng Chen: Graphitic carbon nitride solid nanofilms for selective and recyclable sensing of Cu2+ and Ag+ in water and serum, <a href="http://pubs.rsc.org/en/content/articlelanding/2014/cc/c4cc06659f#!divAbstract">Chem. Commun., 50: 15415-15418 (2014)</a></li>
+							<br>
 
         					<li>Yingzhi Xu, He Li, Ying-Hua Jin, Jun Fan* and Fei Sun: Dimerization Interface of 3-Hydroxyacyl-CoA Dehydrogenase Tunes the Formation of Its Catalytic Intermediate, <a href="http://www.plosone.org/article/info%3Adoi%2F10.1371%2Fjournal.pone.0095965">PLoS ONE, 9: e95965 (2014)</a></li>
-
+							<br>
+							
         					<li>Jun Fan, Marissa G. Saunders, Enrique M. De La Cruz, Gregory A. Voth:  Molecular origins of cofilin-induced actin filament mechanics, <a href="http://www.sciencedirect.com/science/article/pii/S0022283613000363">J. Mol. Biol., 425: 1225 (2013) </a></li>
+							<br>
 
         					<li>Jun Fan, Marissa G. Saunders, Gregory A. Voth: Coarse-graining provides insights on the essential nature of heterogeneity in actin filaments, <a href="http://www.ncbi.nlm.nih.gov/pubmed/22995506"> Biophys. J., 103: 1334 (2012) </a></li>
+							<br>
 
         					<li>Jun Fan, Tao Han, Mikko Haataja: Hydrodynamic Effects on Spinodal Decomposition Kinetics in Lipid Bilayer Membranes, <a href="http://jcp.aip.org/resource/1/jcpsa6/v133/i23/p235101_s1">J. Chem. Phys., 133: 235101 (2010) </a></li>
+							<br>
 
         					<li>Jun Fan, Maria Sammalkorpi, Mikko Haataja: Lipid microdomains: Structural correlations, Fluctuations, and Formation Mechanisms, <a href="http://prl.aps.org/abstract/PRL/v104/i11/e118101"> Phys. Rev. Lett., 104: 118101 (2010)</a></li>
+							<br>
 
         					<li>Jun Fan, Maria Sammalkorpi, Mikko Haataja: Formation and Regulation of Lipid Microdomains in Cell Membranes: Theory, Modeling, and Speculation, <a href="http://www.ncbi.nlm.nih.gov/pubmed/19854186">FEBS Lett., 584: 1678 (2010)</a> (invited review paper)</li>
+							<br>
 
         					<li>Jun Fan, Maria Sammalkorpi, Mikko Haataja: Influence of non-equilibrium lipid transport, membrane compartmentalization, and membrane proteins on the lateral organization of the plasma membrane, <a href="http://pre.aps.org/abstract/PRE/v81/i1/e011908"> Phys. Rev. E., 81:011908 (2010) </a></li>
+							<br>
 
                             <li>Juan Meng, Jun Fan, et al: LHRH-functionalized superparamagnetic iron oxide nanoparticles for breast cancer targeting and contrast enhancement in MRI, <a href="http://www.sciencedirect.com/science/article/pii/S0928493108002555"> Mater. Sci. Eng., C, 29:1467-1479 (2009) </a></li>
+							<br>
 
         					<li>Jun Fan and Maria Sammalkorpi, Mikko Haataja: Domain Formation in the Plasma Membrane: Roles of Non-equilibrium Lipid Transport and Membrane Proteins, <a href="http://prl.aps.org/abstract/PRL/v100/i17/e178102">Phys. Rev. Lett., 100: 178102 (2008)</a></li>
-                            
+							<br>
+							
         					<li>Jun Fan, Michael Greenwood, Mikko Haataja and Nikolas Provatas: Phase-field simulations of velocity selection in rapidly solidified binary alloys, <a href="http://pre.aps.org/abstract/PRE/v74/i3/e031602"> Phys. Rev. E., 74: 031602 (2006)</a> </li>
     					</ol>
 					</div>
