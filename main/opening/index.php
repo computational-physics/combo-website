fd826fa5ab82dfb8077f2a5322ae393fa8227d5b
Zhen Li <33314620+truth-zhenli@users.noreply.github.com>
2018-06-01 16:56:51 +0800

    opening update

--- main/opening/index.php
+++ main/opening/index.php
@@ -25,16 +25,17 @@
 	                        <li class="list-group-item">
 	                            <h4 class="list-group-item-heading">You are encouranged to apply PhD program:</h4>
 	                            <div class="list-group-item-text">
-	                                <p>As long as your English is qualified, you may apply. The chance will increase with a high GPA (> 85 preferred, a low GPA may result in a academic score of 0) and scientific papers published (SCI, top 50% in category).</p>
+	                                <p>As long as your English is qualified, you may apply. Better opportunity can be achieved with a high GPA and/or one or two peer-reviewed international journal papers published (Must be SCI and top 50% in category).</p>
 	                            </div>
 	                        </li>
 	                        <li class="list-group-item">
 	                            <h4 class="list-group-item-heading">English Proficiency Requirements of College of Science and Engineering:</h4>
 	                            <div class="list-group-item-text">
 	                            	<ul>
-	                            		<li>a minimum TOEFL score of 600 (paper-based) or 213 (computer-based) or 95 (internet-based); or</li>
+	                            		<li>a minimum TOEFL score of 550 (paper-based) or 213 (computer-based) or 79 (internet-based); or</li>
 	                            		<li>a minimum overall band score of 6.5 in IELTS; or</li>
-	                            		<li>other test scores that may be regarded as equivalent to TOEFL 600 (paper-based) or 213 (computer-based) or 90 (internet-based).</li>
+										<li>a score of 490 in the Chinese mainland’s College English Test Band 6; or</li>
+	                            		<li>other test scores that may be regarded as equivalent to TOEFL 550 (paper-based) or 213 (computer-based) or 79 (internet-based).</li>
 	                            	</ul>
 	                            	<small>(Please note that individual Departments in the College may adopt a higher standard.)</small>
 	                            </div>
@@ -42,7 +43,7 @@
 	                        <li class="list-group-item">
 	                            <h4 class="list-group-item-heading">Financial Support for Graduate Students:</h4>
 	                            <div class="list-group-item-text">
-	                                <p>Hong Kong govement provide a stipend of about <strong>HKD 16,000</strong> per month (<strong>HKD 24,000</strong> per month for <a target="_blank" href="https://cerg1.ugc.edu.hk/hkpfs/index.html" target="_blank">HKPFS</a>), 12 months per year, to all successful candidates. Additional tuition scholarship and travel fund will be possible for students with great academic performance.</p>
+	                                <p>Hong Kong govement provide a stipend of <strong>> HKD 16,000</strong> per month (<strong>HKD 20,000</strong> per month for <a target="_blank" href="https://cerg1.ugc.edu.hk/hkpfs/index.html" target="_blank">HKPFS</a>), 12 months per year, to all successful candidates. Additional tuition scholarship and travel fund will be possible for students with great academic performance.</p>
 	                            </div>
 	                        </li>
 	                    </ul>
