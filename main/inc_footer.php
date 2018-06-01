5749050a2a1662483c39d80c86723bbc97c71ed3
Zhen Li <33314620+truth-zhenli@users.noreply.github.com>
2018-06-01 21:15:18 +0800

    foot update

--- main/inc_footer.php
+++ main/inc_footer.php
@@ -3,15 +3,11 @@ define("ROOTPATH", "/main");
 ?>
     <div class="footer container">
         <div class="row">
-            <div class="col-xs-12">
-                <img src="//template.cityu.edu.hk/template/img/logos/cityu_deptpage.png">
-                <a href="//www.ap.cityu.edu.hk/cityu/privacy/index.htm">Privacy Policy</a>
-                | <a href="//www.ap.cityu.edu.hk/cityu/copyright/index.htm#copyright">Copyright</a>
-                | <a href="//www.ap.cityu.edu.hk/cityu/disclaimer/index.htm#disclaimer">Disclaimer</a>
-                | <a href="//www.ap.cityu.edu.hk/cityu/accessibility/">Accessibility</a>
-                | <a href="//wikisites.cityu.edu.hk/sites/upolicies/corpid/Wiki%20Pages/University%20Policy%20and%20Guidelines%20for%20the%20Use%20of%20the%20University's%20Name%20and%20Visual%20Identity.aspx">University Logo/Name Policy</a>
-                | <a href="//www.ap.cityu.edu.hk/cityu/geninfo/siteinfo.htm#webmaster">Contact Us</a>
-                | <a href="//www.adobe.com/" target="_blank">Download Acrobat Reader</a>
+            <div class="col-xs-12 vertical-center">
+                <a href="//www6.cityu.edu.hk/mse">Department of Materials Science and Engineering</a>
+                | <a href="//www.cityu.edu.hk/">City University of Hong Kong</a>
+                | <a>Kowloon, Hong Kong SAR</a>
+                | <a href="mailto:junfan@cityu.edu.hk">junfan@cityu.edu.hk</a>
             </div>
         </div>
     </div>
