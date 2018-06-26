b6c4c8e6bd7a93ef5f5cbebcbd8301cc9f37c281
Zhen Li <33314620+truth-zhenli@users.noreply.github.com>
2018-06-26 09:59:12 +0800

    research update

--- main/research/index.php
+++ main/research/index.php
@@ -100,7 +100,7 @@
 					<div class="col-sm-12">
 						<p style="text-align: justify"><b>
 							Peripheral membrane proteins and transmembrane proteins could influence the physical state and functions of lipids, and vice versa.
-							We aim to explore the relationship or coupling between proteins and lipids, including the membrane remodeling by BAR (Bin-Amphiphysin-Rvs) family proteins, 
+							Utilizing molecular dynamics (MD) simulations and other computational analyses, we aim to explore the relationship or coupling between proteins and lipids, including the membrane remodeling by BAR (Bin-Amphiphysin-Rvs) family proteins, 
 							the reversible gating of protein channels stimulated by the lateral pressure of lipid membranes, etc.
 						</b></p>
 						<p style="text-align: justify">
@@ -144,38 +144,27 @@
 						<legend> Bio-Nano Interactions </legend>
 					</div>
 					<div class="col-sm-12">
-						<!--
 						<p style="text-align: justify">
-							Peripheral membrane proteins and transmembrane proteins could influence the physical state and functions of lipids, and vice versa.
-							We aim to explore the relationship or coupling between proteins and lipids, including the membrane remodeling by BAR (Bin-Amphiphysin-Rvs) family proteins, 
-							the reversible gating of protein channels stimulated by the lateral pressure of lipid membranes, etc.
+							As the rapid development of nanomaterials and their emerging biomedical and biological applications such as drug/gene delivery and nanosensing, 
+							the interaction between nanomaterials and biological entities plays an increasingly important role in nanoscience. In particular, bio-nano interactions could interfere functions of proteins, lipids, 
+							etc., and even damage the structural integrity of cells. Thus, the cytotoxicity of nanomaterials has caused serious concerns over the safety and sustainability of nanotechnology. 
+							On the other hand, the toxicity of nanomaterials can also be utilized in antibacterial and antifouling applications.
+							We perform MD simulations to study the interaction between nanoparticles/nanotubes/nanosheets and lipid membranes/proteins/DNA/organelles, providing insights into physics in bio-nano systems. 
 						</p>
 						<p style="text-align: justify">
-							The BAR (Bin-Amphiphysin-Rvs) domain undergoes dimerization to produce a curved protein structure, 
-							which superimposes onto membrane through electrostatic interactions to sense and impart membrane curvature. 
-							In some cases, a BAR domain also possesses an amphipathic helix that inserts into the membrane to induce 
-							curvature. ACAP1 (Arfgap with Coil coil, Ankyrin repeat, and PH domain protein 1) contains a BAR domain. 
-							Here, we show that this BAR domain can neither bind membrane nor impart curvature, 
-							but instead requires a neighboring PH (Pleckstrin Homology) domain to achieve these functions. 
-							Specific residues within the PH domain are responsible for both membrane binding and curvature generation. 
-							The BAR domain adjacent to the PH domain instead interacts with the BAR domains of neighboring ACAP1 proteins to enable clustering at the membrane. 
-							Thus, we have uncovered the molecular basis for an unexpected and unconventional collaboration between PH and BAR domains in membrane bending.
+							We have studied the interaction between 2D nanomaterials (nanosheets) and lipid membranes recently. Hydrophobic nanosheets could extract lipids and then insert into lipid membranes, which is very important 
+							for the cytotoxicity of nanosheets. Interestingly, we found that the lipid extraction behavior depends on the phase state of lipids that can be changed by temperature, lipid components, etc. In addition, 
+							the insertion of nanosheets into lipid membranes could lead to the ordering of lipids, which may perturb the function of transmembrane proteins.
 						</p>
 						
-						<img class="img-responsive" src="../img/research/MembProtein.jpg" />
+						<img class="img-responsive" src="../img/research/BioNano.jpg" />
 
-						<ul>
-                        	<li>Chun Chan, Lanyuan Lu, Fei Sun, Jun Fan*, <u><a href="http://pubs.acs.org/doi/abs/10.1021/acs.jpcb.6b09563">Molecular Details of the PH Domain of ACAP1 BAR-PH Protein Binding to PIP-Containing Membrane</a></u>, <b><i>J. Phys. Chem. B</i></b>, 121: 3586-3596 (2017)</li>						
-                            <li>Chun Chan, Haohua Wen, Lanyuan Lu, and Jun Fan*, <u><a href="http://cpb.iphy.ac.cn/EN/abstract/abstract66287.shtml">Multiscale molecular dynamics simulations membrane remodeling by BAR family proteins</a></u>, <b><i>Chinese Physics B</i></b>, 25: 018707 (2016), invited review</li>						
-        					<li>Xiaoyun Pang, Jun Fan, Yan Zhang, Kai Zhang, Bingquan Gao, Jun Ma, Jian Li, Yuchen Deng, Qiangjun Zhou, Edward H. Egelman, Victor W. Hsu, Fei Sun. <u><a href="http://www.sciencedirect.com/science/article/pii/S1534580714005504">A PH Domain in ACAP1 Possesses Key Features of the BAR Domain in Promoting Membrane Curvature</a></u>, <b><i>Dev. Cell</i></b>, 31: 73-86 (2014)
-                                <i>Highlighted in <a href="http://www.sciencedirect.com/science/article/pii/S1534580714006224">A Novel Twist in Membrane dePHormation</a></i>
-                            </li>
+						<ul style="text-align: justify">
+							<li>Yonghui Zhang, Chun Chan, Zhen Li, Jia Le Ma, Qiangqiang Meng, Xiaolin Cheng, and Jun Fan*, <u>Lipid Extraction by Boron Nitride Nanosheets from Liquid-ordered and Liquid-disordered Nanodomains</u>, <b><i>Nanoscale</i></b>, Accepted (2018)</li>
+                            <li>Zhen Li, Yonghui Zhang, Chun Chan, Chunyi Zhi, Xiaolin Cheng, and Jun Fan*, <u><a href="https://pubs.acs.org/doi/10.1021/acsnano.7b09095">Temperature-Dependent Lipid Extraction from Membranes by Boron Nitride Nanosheets</a></u>, <b><i>ACS Nano</i></b>, 12: 2764-2772 (2018) </li>
+                            <li>Yonghui Zhang, Zhen Li, Chun Chan, Jiale Ma, Chunyi Zhi, Xiaolin Cheng, and Jun Fan*, <u><a href="http://pubs.rsc.org/en/content/articlelanding/2017/cp/c7cp07136a#!divAbstract">Ordering of Lipid Membranes Altered by Boron Nitride Nanosheet</a></u>, <b><i>Phys. Chem. Chem. Phys.</i></b>, 20: 3903-3910 (2018), inside cover</li>
 						</ul>
-						-->
-						<p style="text-align: justify">
-							under construction
-						</p>
-						
+				
 					</div>
 				</div>
 				
@@ -223,7 +212,7 @@
 						</ul>
 						-->
 						<p style="text-align: justify">
-							under construction
+							coming soon ...
 						</p>
 						
 					</div>
